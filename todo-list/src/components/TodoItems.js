f62e84bbf6f5eb27051eb792d6e1c27f4390c7a1
iryna18artemenko <136636205+iryna18artemenko@users.noreply.github.com>
2023-10-31 17:53:35 +0200

    home.work44

---
+++ todo-list/src/components/TodoItems.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { useRef } from "react";
+
+const TodoItem = (props) => {
+  const { item, updateTodo, removeTodo, completeTodo } = props;
+
+  const inputRef = useRef(true);
+
+  const changeFocus = () => {
+    inputRef.current.disabled = false;
+    inputRef.current.focus();
+  };
+
+  const update = (id, value, e) => {
+    if (e.wich === 13) {
+      updateTodo({ id, item: value });
+      inputRef.current.disabled = false;
+    }
+  };
+
+  return (
+    <li key={item.id} className="card">
+      <textarea
+        ref={inputRef}
+        disabled={inputRef}
+        defaultValue={item.item}
+        onKeyUpCapture={(e) => update(item.id, inputRef.current.value, e)}
+      />
+      <div className="btns">
+        <button style={{ color: "blue" }} onClick={() => changeFocus()}>
+          Edit
+        </button>
+        <button
+          style={{ color: "green" }}
+          onClick={() => completeTodo(item.id)}
+        >
+          Complete
+        </button>
+        <button style={{ color: "red" }} onClick={() => removeTodo(item.id)}>
+          Delete
+        </button>
+      </div>
+      {item.completed === true && <p className="completed">done</p>}
+    </li>
+  );
+};
+
+export default TodoItem;
